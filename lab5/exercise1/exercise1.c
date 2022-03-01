ab6f3eaa69d8ef1f2eefdc30680a2013ee17c2a1
Zeulewan <55005377+Zeulewan@users.noreply.github.com>
2022-03-01 15:27:02 +0000

    added new folder

---
+++ lab5/exercise1/exercise1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+  int i = 0;
+  int a = 0.0;
+  while(i<10)
+  {
+    scanf("\n%d\n",&a);
+    printf("%d",a);
+    i++
+  }
+
+
+  return 0;
+}
--- lab5/tests/test.c
+++ lab5/tests/test.c
@@ -5,7 +5,7 @@ int main ()
 
     for(int i=0; i<10;i++)
     {
-        printf("i love u ");
+        printf("hi ");
     }
 //this does nothing
     return 0;
