2d46769b835195220fc16eeca825bffba52f588f
Zeulewan <55005377+Zeulewan@users.noreply.github.com>
2022-03-01 23:07:11 +0000

    nope

--- lab5/tests/test.c
+++ lab5/tests/test.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
 
 int main ()
 {
-
-    for(int i=0; i<10;i++)
-    {
-        printf("hi ");
-    }
-//this does nothing
+  FILE *fileName;
+  char ch[100];
+  fileName = fopen("hi.txt", "r");
+  printf("%s", fgets(ch,50,fileName));
+  fclose(fileName);
+  
+  
     return 0;
 }
