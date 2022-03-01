508f69411bd7ee90ef9d067fd54b70820a47b710
Zeulewan <55005377+Zeulewan@users.noreply.github.com>
2022-03-01 18:48:04 +0000

    secret

--- lab5/exercise1/exercise1.c
+++ lab5/exercise1/exercise1.c
@@ -7,15 +7,15 @@ int main()
   int num_samples;
   int num_orgs_per_100;
 
-  printf("enter &b_num, &num_samples ,num_orgs_per_100");
-  scanf("%d %d %d",&b_num, &num_samples ,&num_orgs_per_100);
+  printf("enter: b_num and num_samples ");
+  scanf("%d %d",&b_num, &num_samples);
 
 
   for(int i=0; i<num_samples;i++)
   {
     int sn;
-    printf("enter sample number\n");
-    scanf ("%d",&sn);
+    printf("enter sample number then click enter\n");
+    scanf ("%d",&num_orgs_per_100);
       
     
 
