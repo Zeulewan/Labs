06114a35284f3c29da750245b94b4fdfde0e01f3
Zeulewan <zeul@mordasiewicz.com>
2022-03-15 18:22:08 -0400

    2 done

---
+++ lab6/2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fact(int y){
+  
+    int x, fact=1;
+
+    for(x=1; x<y; x++){
+        fact = fact*x; 
+    }
+    return (fact);
+}
+
+int main() {
+
+    printf ("The sum of the series is : %d", fact(1)/1+fact(2)/2+fact(3)/3+fact(4)/4+fact(5)/5);
+
+    return 0;
+}
